b815917ac904fdf7804c009673272181e4ce1be3
Merve Nur <mervenurcalmasur@gmail.com>
2026-05-13 22:26:09 +0300

    render-build dosyasi eklendi

--- render-build.sh
+++ render-build.sh
@@ -7,3 +7,12 @@ pip install -r requirements.txt
 
 # LibreOffice'i sistemden değil, taşınabilir bir yolla kullanacağız veya 
 # Render'ın kendi kütüphanelerine güveneceğiz.
+
+# LibreOffice kurulumu (Daha hızlı ve stabil bir repo kullanıyoruz)
+mkdir -p /opt/render/project/src/libreoffice
+cd /opt/render/project/src/libreoffice
+
+# Eğer daha önce indirilmediyse LibreOffice'i çek
+if [ ! -d "libreoffice" ]; then
+  curl -L https://github.com/vsls-contrib/libreoffice-bin/raw/master/libreoffice.tar.gz | tar xz
+fi
